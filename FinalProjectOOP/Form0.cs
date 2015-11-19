e120818c091f77716df073c6a94a77218477e6ae
Joe Temte <jtemte3@yahoo.com>
2015-11-19 11:47:28 -0600

    Added About window, New Request Button which leads to the new request form(form1), Load Request Button which is null, Menu banner which includes as of the current update a submit/load requests and about button under the File tab, and a Manage Technitians button under the Manage tab.

---
+++ FinalProjectOOP/Form0.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectOOP
+{
+    public partial class Form0 : Form
+    {
+        public Form0()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About form = new About();
+            form.ShowDialog();
+        }
+    }
+}
--- FinalProjectOOP/Form1.cs
+++ FinalProjectOOP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FinalProjectOOP
 {
@@ -15,6 +16,26 @@ namespace FinalProjectOOP
         public Form1()
         {
             InitializeComponent();
+            comboBox2.Items.Insert(0, "Joe Temte");
+            comboBox2.Items.Insert(1, "Jessica Livingston");
+            comboBox2.Items.Insert(2, "Gavin Anthony");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Date = textBox8.Text;
+            string Time = textBox7.Text;
+            string Type = comboBox1.Text;
+            string Fname = textBox3.Text;
+            string Lname = textBox4.Text;
+            string Pnum = textBox5.Text;
+            string Email = textBox6.Text;
+            string Problem = textBox1.Text;
+            string Resolution = textBox2.Text;
+            string Tech = comboBox2.Text;
+            int ID = 00002;
+
+            File.WriteAllText(ID +".txt", Date + Environment.NewLine + Time + Environment.NewLine + Type + Environment.NewLine + Fname + Environment.NewLine + Lname + Environment.NewLine + Pnum + Environment.NewLine + Email + Environment.NewLine + Problem + Environment.NewLine + Resolution + Environment.NewLine + Tech + Environment.NewLine + ID);
         }
     }
 }
